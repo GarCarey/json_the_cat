da7a039c17c92d4876bf8962631c5de57a90531a
Gareth <gar.carey@gmail.com>
2021-08-21 17:11:21 -0400

    refactor to add function and use Mocha/Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,23 @@
-const breed = process.argv.slice(2);
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, repsonse, body) => {
-  const data = JSON.parse(body);
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, repsonse, body) => {
+    //return error details if there is an error
+    if (error) {
+      callback(error,  null);
+    }
+    
+    const data = JSON.parse(body);
 
-  //check to see if a valid cat breed was searched
-  if (!data.length) {
-    console.log("Breed not found! Please use a valid breed.");
-    process.exit();
-  }
+    //check to see if a valid cat breed was searched
+    if (!data.length) {
+      callback(error, null);
+    } else {
+      //callback the description of the cat
+      callback(null, data[0].description);
+    }
+  });
+};
 
-  //return error details if there is an error
-  if (error) return ('Error: ', error);
 
-  //print the description of the cat
-  console.log(data[0].description);
-
-});
+module.exports = { fetchBreedDescription };
